a8c9bdc02f27a5fdf226a85d456b2ad2a59d9a41
Khizar <solkarkhizar27@gmail.com>
2024-12-10 17:07:59 +0530

    full stack app

---
+++ backend/server.js
@@ -0,0 +1,72 @@
+import express from 'express'
+
+const app=express();
+
+// app.get('/',(req,res)=>{
+//     res.send('server is ready');
+// });
+
+//get a list  of 5 jokes
+
+app.get('/api/jokes',(req,res)=>{
+  const jokes=  [
+        {
+          "id": 1,
+          "title": "Why don’t scientists trust atoms?",
+          "content": "Because they make up everything!"
+        },
+        {
+          "id": 2,
+          "title": "Why did the scarecrow win an award?",
+          "content": "Because he was outstanding in his field!"
+        },
+        {
+          "id": 3,
+          "title": "Why don’t skeletons fight each other?",
+          "content": "They don’t have the guts."
+        },
+        {
+          "id": 4,
+          "title": "What do you call fake spaghetti?",
+          "content": "An impasta!"
+        },
+        {
+          "id": 5,
+          "title": "Why did the bicycle fall over?",
+          "content": "Because it was two-tired!"
+        },
+        {
+          "id": 6,
+          "title": "What do you call cheese that isn’t yours?",
+          "content": "Nacho cheese!"
+        },
+        {
+          "id": 7,
+          "title": "Why couldn’t the leopard play hide and seek?",
+          "content": "Because he was always spotted."
+        },
+        {
+          "id": 8,
+          "title": "Why did the math book look sad?",
+          "content": "Because it had too many problems."
+        },
+        {
+          "id": 9,
+          "title": "What do you get when you cross a snowman and a dog?",
+          "content": "Frostbite!"
+        },
+        {
+          "id": 10,
+          "title": "Why was the computer cold?",
+          "content": "It left its Windows open!"
+        }
+      ]
+      
+      res.send(jokes);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Serve at http://localhost:${port}`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React, {useState, useEffect } from 'react' 
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [joke,setjokes]=useState([]);
+
+useEffect(()=>{
+  axios.get("/api/jokes")
+  .then((response)=>{
+    setjokes(response.data)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+},[])
+
+  return (
+   <>
+    <h1> chai and full stack </h1>
+    <p>Jokes : {joke.length}</p>
+
+   
+
+    {
+      joke.map((joke)=>(
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))
+    }
+
+
+   </>
+  )
+}
+
+export default App
