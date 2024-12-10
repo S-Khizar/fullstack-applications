f70a822c3e38c38fe5365ddd3c82f36d3cc93a2d
Khizar <solkarkhizar27@gmail.com>
2024-12-10 21:39:57 +0530

    full stack app6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,35 @@
-import React, {useState, useEffect } from 'react' 
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [joke,setjokes]=useState([]);
+  const [joke, setjokes] = useState([]);
 
-useEffect(()=>{
-  axios.get("/api/jokes")
-  .then((response)=>{
-    setjokes(response.data)
-  })
-  .catch((error)=>{
-    console.log(error)
-  })
-},[])
+  useEffect(() => {
+    axios.get("https://fullstack-applications.vercel.app/api/jokes")
+      .then((response) => {
+        setjokes(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
 
   return (
-   <>
-    <h1> chai and full stack </h1>
-    <p>Jokes : {joke.length}</p>
-
-   
-
-    {
-      joke.map((joke)=>(
-        <div key={joke.id}>
-          <h3>{joke.title}</h3>
-          <p>{joke.content}</p>
-        </div>
-      ))
-    }
+    <>
+      <h1> chai and full stack </h1>
+      <p>Jokes : {joke.length}</p>
+      {
+        joke.map((joke) => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
 
 
-   </>
+    </>
   )
 }
 
