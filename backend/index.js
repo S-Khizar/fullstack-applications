a4016d198426e2e5b36e0035d04223b307428df6
Khizar <solkarkhizar27@gmail.com>
2024-12-10 20:07:04 +0530

    full stack app4

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,9 @@ import express from 'express'
 
 const app=express();
 
-// app.get('/',(req,res)=>{
-//     res.send('server is ready');
-// });
+app.get('/',(req,res)=>{
+    res.send('server is ready');
+});
 
 //get a list  of 5 jokes
 
